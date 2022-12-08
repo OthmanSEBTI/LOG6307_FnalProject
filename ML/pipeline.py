ccdfeb97d7ba2faa53d70ee32a5d8eed4089998a
Youssef B <60917844+khlala@users.noreply.github.com>
2022-12-08 11:11:20 -0500

    Code Cleanup V2

--- ML/pipeline.py
+++ ML/pipeline.py
@@ -4,7 +4,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import classification_report,  roc_auc_score
 
 
 
@@ -23,13 +23,25 @@ def process_data(path):
 	classifier = KNeighborsClassifier(n_neighbors=3)
 	classifier.fit(X_train,y_train)
 	y_pred = classifier.predict(X_test)
+	metrics = classification_report(y_test,y_pred)
+	roc = roc_auc_score(y_test,y_pred)
 
-	return classification_report(y_test,y_pred)
+	return metrics, roc
 
-f = open("result.txt", "w")
+
+
 to_process = ['IST_MIR.csv','IST_MOZ.csv','IST_OST.csv','IST_WIK.csv']
+l = ['Results/mirantis_results.txt','Results/moz_results.txt','Results/ost_results.txt','Results/wiki_results.txt']
+
 for line in to_process:
-	p = process_data(line)
-	f.write(line)
-	f.write(p)
-	f.write('\n')
+	for v in l:
+		f = open(v, "w")
+		p, r = process_data(line)
+		for i in range(0, 10):
+			f.write(line)
+			f.write('\n')
+			f.write(p)
+			f.write('\n')
+			f.write('ROC-AUC :' + str(r))
+			f.write('\n')
+
